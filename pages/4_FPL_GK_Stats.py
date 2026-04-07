e790a6a1a7d36fa961ee72444bb8b1bf64423233
Yuriith <y_savenkovh@yahoo.co.uk>
2026-04-07 17:47:37 +0300

    Page created

---
+++ pages/4_FPL_GK_Stats.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="FPL Goalkeepers Stats", layout="wide")
+
+st.title("FPL Goalkeepers Stats 📊")
+st.info("### 🏗️ Work In Progress")
+st.write("This page is currently under development. Come back soon!")
+
+if st.button("Back to Main"):
+    st.switch_page("FPL_Main_Info.py")
