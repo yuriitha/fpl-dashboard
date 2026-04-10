d2f87a944079646cb86fee9a26fa2d7389af5e49
Yuriith <y_savenkovh@yahoo.co.uk>
2026-04-10 07:56:38 +0300

    Page created

---
+++ pages/8_UCL_Defensive_Stats.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="UCL Defensive Stats", layout="wide")
+
+st.title("UCL Defensive Stats 📊")
+st.info("### 🏗️ Work In Progress")
+st.write("This page is currently under development. Come back soon!")
+
+if st.button("Back to Main"):
+    st.switch_page("FPL_Main_Info.py")
