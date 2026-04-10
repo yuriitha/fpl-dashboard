b63b46ebd76b1172bea5999932bc4a46969e3b69
Yuriith <y_savenkovh@yahoo.co.uk>
2026-04-10 08:05:29 +0300

    Page created

---
+++ pages/10_UCL_Players_Form.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="UCL Players", layout="wide")
+
+st.title("UCL Players Form 📊")
+st.info("### 🏗️ Work In Progress")
+st.write("This page is currently under development. Come back soon!")
+
+if st.button("Back to Main"):
+    st.switch_page("FPL_Main_Info.py")
